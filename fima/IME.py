aa322087982cb254259adaec310a11a278a34761
hemoboghosian <HemoBoghosian@gmail.com>
2025-05-11 22:46:59 +0330

    Implementing get_all_ime_option_trades function...

--- fima/IME.py
+++ fima/IME.py
@@ -144,4 +144,77 @@ def get_all_ime_futures_trades(only_active: bool = False, start_date: str = None
     return pd.DataFrame(all_rows)
 
 
+def get_all_ime_option_trades(option_type: str = 'All', only_active: bool = False, start_date: str = None,
+                               end_date: str = None, _chunk_size: int = 100, _offset: int = 0) -> pd.DataFrame:
+
+    if only_active:
+        start_date = str(jd.date.today() - jd.timedelta(days=10))
+        end_date = str(jd.date.today())
+    else:
+        if start_date is None:
+            start_date = '1385-01-01'
+        if end_date is None:
+            end_date = str(jd.date.today())
+
+    temp_start_date = jd.date(year=int(start_date[:4]), month=int(start_date[5:7]), day=int(start_date[8:]))
+    end_date = temp_end_date = jd.date(year=int(end_date[:4]), month=int(end_date[5:7]), day=int(end_date[8:]))
+    chunked_dates = []
+    while temp_end_date <= end_date:
+        temp_end_date = temp_start_date + jd.timedelta(days=_chunk_size)
+        chunked_dates.append([str(temp_start_date).replace('-', '/'), str(temp_end_date).replace('-', '/')])
+        temp_start_date = temp_end_date + jd.timedelta(days=1)
+        temp_end_date = temp_end_date + jd.timedelta(days=_chunk_size)
+    chunked_dates.append([str(temp_start_date + jd.timedelta(days=1)).replace('-', '/'), str(end_date).replace('-', '/')])
+
+    url = "https://www.ime.co.ir/subsystems/ime/option/optionboarddata.ashx"
+    contract_filter = -1 if only_active else 0
+
+    if option_type == 'Call':
+        option_type_filter = 1
+    elif option_type == 'Put':
+        option_type_filter = 2
+    else:
+        option_type_filter = 0
+
+    headers = {"User-Agent": "Mozilla/5.0", "Accept": "application/json, text/javascript, */*; q=0.01",
+               "X-Requested-With": "XMLHttpRequest", "Referer": "https://www.ime.co.ir/fut-report.html"}
+
+    all_rows = []
+    for f, t in chunked_dates:
+        params = {"f": f, "t": t, "c": contract_filter, "ot": option_type_filter, "lang": 8, "order": "asc"}
+
+        try:
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            data = response.json()
+            rows = data.get("rows", [])
+            if rows:
+                all_rows.extend(rows)
+        except Exception as e:
+            print(f"❌ Failed for chunk {f} to {t}: {e}")
+            continue
+    if all_rows:
+        all_data = pd.DataFrame(all_rows)
+        all_data.rename({'DT': 'Date', 'Vol_Haghighi_Buy': 'RetailBuyVolume', 'Val_Haghighi_Buy': 'RetailBuyValue',
+                         'Vol_Haghighi_Sell': 'RetailSellVolume', 'Val_Haghighi_Sell': 'RetailSellValue',
+                         'Vol_Hoghooghi_Buy': 'InstitutionalBuyVolume', 'Val_Hoghooghi_Buy': 'InstitutionalBuyValue',
+                         'Vol_Hoghooghi_Sell': 'InstitutionalSellVolume', 'Val_Hoghooghi_Sell': 'InstitutionalSellValue',
+                         'C_Buy': 'CBuy', 'C_Sell': 'CSell', 'id': 'ID', 'DT_en': 'GDate'},
+                        inplace=True, axis=1)
+
+        all_data['Date'] = all_data['Date'].apply(
+            lambda str_j_date: jd.date(year=int(str_j_date[:4]), month=int(str_j_date[5:7]), day=int(str_j_date[8:]))
+            if pd.notna(str_j_date) else None)
+        all_data['DeliveryDate'] = all_data['DeliveryDate'].apply(
+            lambda str_j_date: jd.date(year=int(str_j_date[:4]), month=int(str_j_date[5:7]), day=int(str_j_date[8:]))
+            if pd.notna(str_j_date) else None)
+        all_data['GDate'] = pd.to_datetime(all_data['GDate'])
+        all_data['GDate'] = all_data['GDate'].dt.date
+        all_data['CreateDateTime'] = pd.to_datetime(all_data['CreateDateTime'], format='mixed')
+        all_data['CreateDateTime'] = all_data['CreateDateTime'].dt.date
+
+        return all_data
+    return pd.DataFrame(all_rows)
+
+
 
--- test/TestIME.py
+++ test/TestIME.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 import jdatetime as jd
-from fima.IME import get_all_ime_physical_trades, get_all_ime_futures_trades
+from fima.IME import get_all_ime_physical_trades, get_all_ime_futures_trades, get_all_ime_option_trades
 
 
 @pytest.mark.parametrize(["start_date", "end_date"], [('1400-01-01', '1400-12-29'),
@@ -47,3 +47,56 @@ def test_get_all_ime_futures_trades(only_active, start_date, end_date):
                 'InstitutionalBuyVolume', 'InstitutionalBuyValue',
                 'MonthlyOpenInterestsPercent', 'MonthlySettlementPricePercent',
                 'InstitutionalSellVolume', 'InstitutionalSellValue'])
+
+
+@pytest.mark.parametrize(["option_type", "only_active", "start_date", "end_date"],
+                         [
+                             ("All", True, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today())),
+                             ("All", True, str(jd.date.today()), str(jd.date.today() + jd.timedelta(days=10))),
+                             ("All", True, str(jd.date.today()), str(jd.date.today())),
+                             ("All", True, str(jd.date.today() - jd.timedelta(days=10)),
+                              str(jd.date.today() + jd.timedelta(days=10))),
+                             ("All", True, None, None),
+                             ("All", False, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today())),
+                             ("All", False, '1404-01-01', '1404-02-01'),
+                             ("All", False, '1404-01-01', str(jd.date.today() + jd.timedelta(days=10))),
+                             ("All", False, None, None),
+                             ("All", False, None, '1400-01-01'),
+                             ("All", False, '1403-01-01', None),
+                             ("Call", True, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today())),
+                             ("Call", True, str(jd.date.today()), str(jd.date.today() + jd.timedelta(days=10))),
+                             ("Call", True, str(jd.date.today()), str(jd.date.today())),
+                             ("Call", True, str(jd.date.today() - jd.timedelta(days=10)),
+                              str(jd.date.today() + jd.timedelta(days=10))),
+                             ("Call", True, None, None),
+                             ("Call", False, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today())),
+                             ("Call", False, '1404-01-01', '1404-02-01'),
+                             ("Call", False, '1404-01-01', str(jd.date.today() + jd.timedelta(days=10))),
+                             ("Call", False, None, None),
+                             ("Call", False, None, '1400-01-01'),
+                             ("Call", False, '1403-01-01', None),
+                             ("Put", True, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today())),
+                             ("Put", True, str(jd.date.today()), str(jd.date.today() + jd.timedelta(days=10))),
+                             ("Put", True, str(jd.date.today()), str(jd.date.today())),
+                             ("Put", True, str(jd.date.today() - jd.timedelta(days=10)),
+                              str(jd.date.today() + jd.timedelta(days=10))),
+                             ("Put", True, None, None),
+                             ("Put", False, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today())),
+                             ("Put", False, '1404-01-01', '1404-02-01'),
+                             ("Put", False, '1404-01-01', str(jd.date.today() + jd.timedelta(days=10))),
+                             ("Put", False, None, None),
+                             ("Put", False, None, '1400-01-01'),
+                             ("Put", False, '1403-01-01', None),
+                         ])
+def test_get_all_ime_option_trades(option_type, only_active, start_date, end_date):
+    all_ime_option_trades = get_all_ime_option_trades(option_type, only_active, start_date, end_date)
+    assert isinstance(all_ime_option_trades, pd.DataFrame)
+    assert all_ime_option_trades is not None
+    assert not all_ime_option_trades.empty
+    assert all(column in all_ime_option_trades.columns for column in
+               ['ID', 'ContractID', 'ContractCode', 'ContractDescription', 'IsActive', 'TradesVolume', 'TradesValue',
+                'MaxPrice', 'MinPrice', 'LastPrice', 'FirstPrice', 'OpenInterest', 'ChangeOpenInterest',
+                'ActiveCustomers', 'ActiveBrokers', 'CBuy', 'CSell', 'InstitutionalBuyVolume', 'InstitutionalBuyValue',
+                'InstitutionalSellVolume', 'InstitutionalSellValue', 'RetailBuyVolume', 'RetailBuyValue',
+                'RetailSellVolume', 'RetailSellValue', 'LastSettlementPrice', 'TodaySettlementPrice',
+                'SettlementPricePercent', 'Date', 'GDate', 'DeliveryDate', 'CreateDateTime'])
