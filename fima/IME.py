bd775882347a570db695530f9276c4d08debb3d6
hemoboghosian <HemoBoghosian@gmail.com>
2025-05-11 21:14:31 +0330

    Finished get_all_ime_futures_trades function.

--- fima/IME.py
+++ fima/IME.py
@@ -82,18 +82,17 @@ def get_all_ime_physical_trades(start_date: str = None, end_date: str = None, _c
     return all_data
 
 
-def get_all_ime_futures_trades(only_active: str = False, start_date: str = None, end_date: str = None, _chunk_size: int = 100, _offset: int = 0) -> pd.DataFrame:
+def get_all_ime_futures_trades(only_active: bool = False, start_date: str = None, end_date: str = None,
+                               _chunk_size: int = 100, _offset: int = 0) -> pd.DataFrame:
 
-    # only_active = False
-    # start_date = '1400-01-01'
-    # end_date = '1400-12-29'
-    # _chunk_size = 100
-    # _offset = 0
-
-    if start_date is None:
-        start_date = '1385-01-01'
-    if end_date is None:
+    if only_active:
+        start_date = str(jd.date.today() - jd.timedelta(days=10))
         end_date = str(jd.date.today())
+    else:
+        if start_date is None:
+            start_date = '1385-01-01'
+        if end_date is None:
+            end_date = str(jd.date.today())
 
     temp_start_date = jd.date(year=int(start_date[:4]), month=int(start_date[5:7]), day=int(start_date[8:]))
     end_date = temp_end_date = jd.date(year=int(end_date[:4]), month=int(end_date[5:7]), day=int(end_date[8:]))
@@ -127,15 +126,12 @@ def get_all_ime_futures_trades(only_active: str = False, start_date: str = None,
             continue
     if all_rows:
         all_data = pd.DataFrame(all_rows)
-        all_data.columns = ['ContractDay', 'ContractCode', 'ContractDescription', 'TradesVolume', 'TradesValue', 'MaxPrice',
-                            'MinPrice', 'LastPrice', 'FirstPrice', 'ChangeOpenInterest', 'ActiveBrokers', 'C_Buy', 'C_Sell',
-                            'InstitutionalBuyVolume', 'InstitutionalBuyValue', 'InstitutionalSellVolume',
-                            'InstitutionalSellValue', 'RetailBuyVolume', 'RetailBuyValue', 'RetailSellVolume',
-                            'RetailSellValue', 'LastSettlementPrice', 'TodaySettlementPrice', 'SettlementPricePercent',
-                            'Date', 'DeliveryDate', 'WeeklyOpenInterests', 'WeeklyOpenInterestsPercent',
-                            'MonthlyOpenInterests', 'MonthlyOpenInterestsPercent', 'WeeklySettlementPrice',
-                            'WeeklySettlementPricePercent', 'MonthlySettlementPrice', 'MonthlySettlementPricePercent',
-                            'OpenInterest', 'ActiveCustomers']
+        all_data.rename({'DT': 'Date', 'Vol_Haghighi_Buy': 'RetailBuyVolume', 'Val_Haghighi_Buy': 'RetailBuyValue',
+                         'Vol_Haghighi_Sell': 'RetailSellVolume', 'Val_Haghighi_Sell': 'RetailSellValue',
+                         'Vol_Hoghooghi_Buy': 'InstitutionalBuyVolume', 'Val_Hoghooghi_Buy': 'InstitutionalBuyValue',
+                         'Vol_Hoghooghi_Sell': 'InstitutionalSellVolume', 'Val_Hoghooghi_Sell': 'InstitutionalSellValue',
+                         'C_Buy': 'CBuy', 'C_Sell': 'CSell'},
+                        inplace=True, axis=1)
 
         all_data['Date'] = all_data['Date'].apply(
             lambda str_j_date: jd.date(year=int(str_j_date[:4]), month=int(str_j_date[5:7]), day=int(str_j_date[8:]))
@@ -148,4 +144,4 @@ def get_all_ime_futures_trades(only_active: str = False, start_date: str = None,
     return pd.DataFrame(all_rows)
 
 
-Test = get_all_ime_futures_trades(only_active=False, start_date='1400-01-01', end_date='1400-12-29')
+
--- test/TestIME.py
+++ test/TestIME.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 import jdatetime as jd
-from fima.IME import get_all_ime_physical_trades
+from fima.IME import get_all_ime_physical_trades, get_all_ime_futures_trades
 
 
 @pytest.mark.parametrize(["start_date", "end_date"], [('1400-01-01', '1400-12-29'),
@@ -18,23 +18,32 @@ def test_get_all_ime_physical_trades(start_date, end_date):
                 'TransactionValue', 'Date', 'DeliveryDate', 'Warehouse', 'Supplier', 'SettlementDate', 'Broker',
                 'SupplyType', 'BuyType', 'Currency', 'Unit', 'ExchangeHall', 'PacketType', 'Settlement'])
 
+
 @pytest.mark.parametrize(["only_active", "start_date", "end_date"],
-                         [(True, '1400-01-01', '1400-12-29'), (True, '1404-01-01', str(jd.date.today() + jd.timedelta(days=10))),
-                          (True, None, None), (True, None, '1400-12-29'), (True, '1400-12-29', None),
-                          (False, '1400-01-01', '1400-12-29'), (False, '1404-01-01', str(jd.date.today() + jd.timedelta(days=10))),
-                          (False, None, None), (False, None, '1400-12-29'), (False, '1400-12-29', None)])
-def test_get_all_ime_physical_trades(only_active, start_date, end_date):
-    all_ime_physical_trades = get_all_ime_physical_trades(start_date, end_date)
-    assert all_ime_physical_trades is not None
-    assert not all_ime_physical_trades.empty
-    assert isinstance(all_ime_physical_trades, pd.DataFrame)
-    assert all(column in all_ime_physical_trades.columns for column in
-               ['ContractDay', 'ContractCode', 'ContractDescription', 'TradesVolume', 'TradesValue', 'MaxPrice',
-                'MinPrice', 'LastPrice', 'FirstPrice', 'ChangeOpenInterest', 'ActiveBrokers', 'C_Buy', 'C_Sell',
-                'InstitutionalBuyVolume', 'InstitutionalBuyValue', 'InstitutionalSellVolume',
-                'InstitutionalSellValue', 'RetailBuyVolume', 'RetailBuyValue', 'RetailSellVolume',
-                'RetailSellValue', 'LastSettlementPrice', 'TodaySettlementPrice', 'SettlementPricePercent',
-                'Date', 'DeliveryDate', 'WeeklyOpenInterests', 'WeeklyOpenInterestsPercent',
-                'MonthlyOpenInterests', 'MonthlyOpenInterestsPercent', 'WeeklySettlementPrice',
-                'WeeklySettlementPricePercent', 'MonthlySettlementPrice', 'MonthlySettlementPricePercent',
-                'OpenInterest', 'ActiveCustomers'])
+                         [(True, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today())),
+                          (True, str(jd.date.today()), str(jd.date.today() + jd.timedelta(days=10))),
+                          (True, str(jd.date.today()), str(jd.date.today())),
+                          (True, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today() + jd.timedelta(days=10))),
+                          (True, None, None),
+                          (False, str(jd.date.today() - jd.timedelta(days=10)), str(jd.date.today())),
+                          (False, '1404-01-01', '1404-02-01'),
+                          (False, '1404-01-01', str(jd.date.today() + jd.timedelta(days=10))),
+                          (False, None, None), (False, None, '1400-01-01'), (False, '1403-01-01', None)])
+def test_get_all_ime_futures_trades(only_active, start_date, end_date):
+    all_ime_futures_trades = get_all_ime_futures_trades(only_active, start_date, end_date)
+    assert isinstance(all_ime_futures_trades, pd.DataFrame)
+    assert all_ime_futures_trades is not None
+    assert not all_ime_futures_trades.empty
+    assert all(column in all_ime_futures_trades.columns for column in
+               ['ContractDay', 'ContractCode', 'ContractDescription', 'TradesVolume',
+                'TradesValue', 'MaxPrice', 'MinPrice', 'LastPrice', 'FirstPrice',
+                'OpenInterest', 'ChangeOpenInterest', 'ActiveCustomers',
+                'ActiveBrokers', 'CBuy', 'CSell', 'RetailBuyVolume', 'RetailBuyValue',
+                'RetailSellVolume', 'RetailSellValue', 'LastSettlementPrice',
+                'TodaySettlementPrice', 'SettlementPricePercent', 'Date',
+                'DeliveryDate', 'WeeklyOpenInterests', 'WeeklyOpenInterestsPercent',
+                'MonthlyOpenInterests', 'WeeklySettlementPrice',
+                'WeeklySettlementPricePercent', 'MonthlySettlementPrice',
+                'InstitutionalBuyVolume', 'InstitutionalBuyValue',
+                'MonthlyOpenInterestsPercent', 'MonthlySettlementPricePercent',
+                'InstitutionalSellVolume', 'InstitutionalSellValue'])
