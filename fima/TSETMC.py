6525be13d28f0ceab03dbb2774f4ce115f416b9a
Hemo Boghosian <hemoboghosian@gmail.com>
2025-06-08 16:47:45 +0330

    get_tickers function...

--- fima/TSETMC.py
+++ fima/TSETMC.py
@@ -4,6 +4,33 @@ import requests
 from persian import convert_ar_characters
 
 
+def _get_static_data() -> (pd.DataFrame, pd.DataFrame):
+    url = "https://cdn.tsetmc.com/api/StaticData/GetStaticData"
+    static_data_combined = requests.get(url).json()['staticData']
+    paper_types = []
+    industrial_groups = []
+    for static_data in static_data_combined:
+        if static_data['type'] == 'IndustrialGroup':
+            industrial_groups.append(static_data)
+        elif static_data['type'] == 'PaperType':
+            paper_types.append(static_data)
+
+    paper_types = pd.DataFrame(paper_types)
+    paper_types['name'] = paper_types['name'].apply(lambda name: convert_ar_characters(name.strip()))
+    # paper_types['description'] = paper_types['description'].apply(lambda description: description.strip())
+    paper_types.drop(['description', 'type'], axis=1, inplace=True)
+    paper_types.rename(columns={'code': 'Code', 'name': 'Instrument', 'id': 'ID'}, inplace=True)
+
+    industrial_groups = pd.DataFrame(industrial_groups)
+    industrial_groups['name'] = industrial_groups['name'].apply(lambda name: convert_ar_characters(name.strip()))
+    # industrial_groups['description'] = industrial_groups['description'].apply(lambda description: description.strip())
+    industrial_groups.drop(['description', 'type'], axis=1, inplace=True)
+    industrial_groups.rename(columns={'code': 'Code', 'name': 'IndustryGroup', 'id': 'ID'}, inplace=True)
+    industrial_groups = industrial_groups[['ID', 'Code', 'IndustryGroup']]
+
+    return paper_types, industrial_groups
+
+
 def get_ticker_historical_data(ticker: str) -> pd.DataFrame:
     ticker_instrument_code = 778253364357513
     url = f"https://cdn.tsetmc.com/api/ClosingPrice/GetClosingPriceDailyList/{ticker_instrument_code}/0"
@@ -16,6 +43,45 @@ def get_ticker_intraday_trades(ticker: str) -> pd.DataFrame:
     url = f"https://cdn.tsetmc.com/api/Trade/GetTrade/{ticker_instrument_code}"
     ticker_intraday_trades = pd.DataFrame(requests.get(url).json()['trade'])
     return ticker_intraday_trades
+
+
+def get_tickers(market: str='All', instrument: str='All', sector: str='All', _based_on: str='Value') -> pd.DataFrame:
+    # market = 'tse'
+    # instrument = 'debt'
+    # sector = 'All'
+    market = market.lower()
+    instrument = instrument.lower()
+    sector = sector.lower()
+    size = 20000
+
+    based_on_dictionary = {'Value': 1, 'Volume': 2, 'MarketCap': 3}
+
+    markets_dictionary = {'ifb': 'Otc', 'tse': 'Tse', 'all': 'All'}
+
+    instruments_dictionary = {'stock': 'Stock', 'debt': 'Debt', 'etf': 'Etf', 'derivative': 'Derivative', 'base': 'Base'}
+
+    _, sectors = _get_static_data()
+    sectors_dictionary = sectors.loc[:, ['Code', 'IndustryGroup']].set_index('IndustryGroup')
+    sectors_dictionary.loc['all', 'Code'] = 0
+    sectors_dictionary['Code'] = sectors_dictionary['Code'].astype(int)
+
+    if market == 'all':
+        url = ("https://cdn.tsetmc.com/api/ClosingPrice/GetMarketMap?"
+               "market=AllAll-TseAll-OtcAll-TseDebt-TseEtf-TseDerivative-TseStock-OtcDebt-OtcEtf-OtcBase-OtcDerivative-OtcStock-&"
+               f"size={size}&sector={sectors_dictionary.loc[sector, 'Code']}&typeSelected={based_on_dictionary[_based_on]}&hEven=0")
+    else:
+        if instrument == 'all':
+            url = ("https://cdn.tsetmc.com/api/ClosingPrice/GetMarketMap?"
+                   f"market={markets_dictionary[market]}All-{markets_dictionary[market]}Stock-"
+                   f"{markets_dictionary[market]}Debt-{markets_dictionary[market]}Etf-{markets_dictionary[market]}Derivative-&"
+                   f"size={size}&sector={sectors_dictionary.loc[sector, 'Code']}&typeSelected={based_on_dictionary[_based_on]}&hEven=0")
+        else:
+            market_combination = f"{markets_dictionary[market]}{instruments_dictionary[instrument]}"
+            url = ("https://cdn.tsetmc.com/api/ClosingPrice/GetMarketMap?"
+                   f"market={market_combination}-&size={size}&sector={sectors_dictionary.loc[sector, 'Code']}&"
+                   f"typeSelected={based_on_dictionary[_based_on]}&hEven=0")
+    data = pd.DataFrame(requests.get(url, timeout=10).json())
+    return data
 
 
 def get_ticker_info(ticker: str) -> pd.DataFrame:
